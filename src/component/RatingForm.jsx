ae47f94bff6b5343072c08d1d01f20f7e5d11f28
Thomas Lucas <t.lucas@lienas.de>
2021-07-30 14:52:43 +0200

    implement Rating -Form using react-hook-form

--- src/component/RatingForm.jsx
+++ src/component/RatingForm.jsx
@@ -1,29 +1,84 @@
-import React, {useState} from 'react';
-import {Button, Form, FormControl} from "react-bootstrap";
-import {useForm, Controller} from "react-hook-form";
-
+import React from 'react';
+import {Button, Col, Form, Row} from "react-bootstrap";
+import {Controller, useForm} from "react-hook-form";
+import {BookRating} from "./BookRating";
+import Rating from "react-rating";
 
 
 const RatingForm = (props) => {
     const {control, handleSubmit} = useForm();
 
+    const TextInput = ({onChange, label}) => {
+        return (
+            <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>{label}</Form.Label>
+                <Form.Control onChange={onChange}/>
+            </Form.Group>
+        )
+    }
+
+    const TextLineInput = ({onChange, value, label}) => {
+        console.log('TextArea = ', value);
+        return (
+            <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>{label}</Form.Label>
+                <Form.Control
+                    as={"textarea"}
+                    style={{height: '200px'}}
+                    value={value}
+                    onChange={onChange}/>
+            </Form.Group>
+        )
+    }
+
+    const StarInput = (field) => {
+        return (
+            <>
+                <Rating
+                    {...field}
+                    initialRating={field.value}
+                    emptySymbol={<BookRating color='white' size={3}/>}
+                    fullSymbol={<BookRating color='teal' size={3}/>}
+                    className={'my-3'}
+                />
+            </>
+        )
+    }
+
     const onSubmit = (data) => {
         console.log('Received data = ', data);
     }
     return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-                control={control}
-                render={({field}) =>
-                    <Form.Group className="mb-3" controlId="formName">
-                    <Form.Label>Dein Name</Form.Label>
-                    <Form.Control {...field}/>
-                </Form.Group>}
-                name={"name"}/>
+        <Row>
+            <Col xl={6}>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <Controller
+                        control={control}
+                        render={({field}) =>
+                            <StarInput {...field}/>
+                        }
+                        name={"rateValue"}
+                    />
+                    <Controller
+                        control={control}
+                        render={({field: {value, onChange}}) =>
+                            <TextInput onChange={onChange} label={'Name'}/>
+                        }
+                        name={"name"}/>
+                    <Controller
+                        control={control}
+                        name={"text"}
+                        render={({field: {value, onChange, ref}}) =>
+                            <TextLineInput onChange={onChange} value={value} label={'Bewertung'}/>
+                        }
+                        
+                    />
 
-            <Button>Bewertung senden</Button>
+                    <Button onClick={handleSubmit(onSubmit)}>Bewertung senden</Button>
 
-        </Form>
+                </Form>
+            </Col>
+        </Row>
     );
 };
 
