0eef03ec1d9f108987cf0c1fb30d2cd6fbe7e7f2
thomas <t.lucas@lienas.de>
2021-07-30 08:50:38 +0200

    implement form-hook

--- src/component/RatingForm.jsx
+++ src/component/RatingForm.jsx
@@ -1,17 +1,26 @@
 import React, {useState} from 'react';
-import {Button, Form} from "react-bootstrap";
+import {Button, Form, FormControl} from "react-bootstrap";
+import {useForm, Controller} from "react-hook-form";
+
+
 
 const RatingForm = (props) => {
-    const [formName, setFormName] = useState()
+    const {control, handleSubmit} = useForm();
+
+    const onSubmit = (data) => {
+        console.log('Received data = ', data);
+    }
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formName">
-                <Form.Label>Dein Name</Form.Label>
-                <Form.Control type="text" placeholder="Name"
-                              onChange={(value) => setFormName(value)}
-                              value={formName}
-                />
-            </Form.Group>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+                control={control}
+                render={({field}) =>
+                    <Form.Group className="mb-3" controlId="formName">
+                    <Form.Label>Dein Name</Form.Label>
+                    <Form.Control {...field}/>
+                </Form.Group>}
+                name={"name"}/>
+
             <Button>Bewertung senden</Button>
 
         </Form>
